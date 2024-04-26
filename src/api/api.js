32754419f7a7a2222e2a021fdb02f0e8835b4483
Dody Setiyawan <dody.setiyawan11@gmail.com>
2024-04-26 09:35:42 +0700

    init commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,10 @@
 const API_BASE_URL = 'https://api.videosdk.live/v2';
 
+// export const getToken = async () => {
+//   return 'VIDEOSDK_DASHBOARD_TOKEN';
+// };
 export const getToken = async () => {
-  return 'VIDEOSDK_DASHBOARD_TOKEN';
+  return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJjYjFmMTY0Yy0zNjFjLTQ2NTItOWUwZC1mOWEyNDJkZjgzMzgiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTcxNDA1OTczOCwiZXhwIjoxNzE0MTQ2MTM4fQ.PUTfNNykwTBLL0Av7cmb0Ou7EfSoWvn74LMWnCCGLRQ"
 };
 
 export const createMeeting = async ({token}) => {
